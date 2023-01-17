1907d2bb4e601880261d3bbe7b62b78c099f27b8
Jeongkyu Lee <jaykay0408@users.noreply.github.com>
2023-01-17 16:14:01 -0500

    Add files via upload

---
+++ lab-assignment2/employee.c
@@ -0,0 +1,34 @@
+/*C Program to read and print the n employee details using structure and dynamic memory allocation*/
+#include <stdio.h>
+#include <stdlib.h>
+/*structure to hold the details of an employee*/
+typedef struct employee
+{
+    int empId;
+    char Name[100];
+    char Designation[100];
+    char Dept[10];
+
+}employee_t;
+/* Read the employee details using scanf*/
+void readData(int n, employee_t* s)
+{
+    // Add your code here
+}
+/* Function to print the employee details*/
+void display(int n, employee_t * s)
+{
+    // Add your code here
+    
+}
+
+/*----------Main Program--------*/
+int main()
+
+{
+    // Main Function to print the employee details
+    // Add your code here
+    
+
+}
+
