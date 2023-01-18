00f30db1c8dd86930caf83cb295eed9409cb0b89
Lixing Chen <chrischenlixing@login-students.khoury.northeastern.edu>
2023-01-17 22:34:02 -0500

    helloworld.c

--- assignment1/helloworld.c
+++ assignment1/helloworld.c
@@ -4,7 +4,7 @@
 // email: jeo.lee@northeastern.edu
 
 int main() {
-  printf("Hello World.  This is [YourName].\n");
-  printf("I am assignment 1 for [This Semester].\n");
+  printf("Hello World.  This is Lixing(Chris) Chen.\n");
+  printf("I am assignment 1 for Spring 2023.\n");
   return 0;
 }
