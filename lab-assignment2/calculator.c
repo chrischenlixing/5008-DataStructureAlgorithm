97c6a79d80dc6cdac2e14bbc42f85f060f45d6dc
Lixing Chen <chrischenlixing@login-students.khoury.northeastern.edu>
2023-01-21 04:13:56 -0500

    calculator

--- lab-assignment2/calculator.c
+++ lab-assignment2/calculator.c
@@ -13,6 +13,35 @@ int main()
     scanf("%d %d", &a, &b);
     
     // Add your code here using switch
-           
+    
+        switch (choice){
+        case 1:
+            c = a+b;
+            printf("Sum = %d",c);
+            break;
+        case 2:
+            c = a-b;
+            printf("Difference = %d",c);
+            break;
+        case 3:
+            c = a*b;
+            printf("Product = %d",c);
+            break;
+        case 4:
+            if (b == 0) {
+                printf("Divide by zero error");
+                break;
+            }else{
+            int quotient = a/b;
+            int remainder = a%b;
+            printf("Quotient = %d\n",quotient);
+            printf("Remainder = %d\n",remainder);
+            break;
+            }
+        default:
+            printf("Invalid option");
+            break;
+
+        }		       
     return 0;
 }
