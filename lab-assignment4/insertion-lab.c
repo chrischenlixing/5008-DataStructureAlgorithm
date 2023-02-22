d034eb88d2358bfe26c27d9f4a47c515dab5dbbb
chrischenlixing <chrischenlixing@gmail.com>
2023-02-22 14:04:43 -0800

    resubmit 4

--- lab-assignment4/insertion-lab.c
+++ lab-assignment4/insertion-lab.c
@@ -1,5 +1,5 @@
-//enter your name here
-//enter your email here
+//enter your name here Chris Chen
+//enter your email here chen.lixin@northeastern.edu
 //To perform insertion sort of integers stored in a file and display the output and store it in another file.
 #include <stdio.h>
 #include<stdlib.h>
@@ -20,10 +20,35 @@ int main()
     
     //insert your code here
     
+    count = sizeof(arr)/sizeof(int);
+
+    for (i=0;i<count;i++){
+        fscanf (fp,"%d",&arr[i]);
+    }
+
+    int key;
+  
+    for (i=1;i<count;i++){
+      key = arr[i];
+      for (j=i-1; j>=0 && arr[j]>key;j--){
+        arr[j+1] = arr[j];
+      }
+      arr[j+1] = key;
+    }
     
-    
-    
-    
+ 
+    FILE *newfp;
+    newfp = fopen("sorted.txt", "w");
+
+    for (i=0;i<count-1;i++){
+        fprintf(newfp,"%d\n",arr[i]);
+    }
+    fprintf(newfp,"%d",arr[count-1]);
+  
+  
+    fclose(fp);
+  
+    fclose(newfp);
     
     
     return 0;
